91226cbfc3a6556abf1e15c4a43d71fa2da3bd47
Paul Pichon F <paul10_barca@hotmail.com>
2024-02-08 18:18:35 -0600

    Create: creacion log.datasources, log.repository

---
+++ src/domain/repository/log.repository.ts
@@ -0,0 +1,10 @@
+// Importamos de log.entity.ts
+import { LogEntity, LogSeverityLevel } from "../entities/log.entity";
+
+//Una clase abstracta nos ayuda a evitar a crear una instancia de la clase LogDatasource, en eset caso.
+// Esto nos ayuda a que nadie pueda crear instancias de nuestra clase LogDatasource directamente.
+export abstract class LogRepository {
+    // metodos
+    abstract saveLog( log: LogEntity): Promise<void>;
+    abstract getLogs( severityLevel: LogSeverityLevel): Promise<LogEntity[]>;
+}
