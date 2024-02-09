d9617685211149a266265ea62aabcbbe0199ba8e
Paul Pichon F <paul10_barca@hotmail.com>
2024-02-09 16:57:58 -0600

    Create: logica de LogRepository

---
+++ src/infrastructure/repositories/log.repository.impl.ts
@@ -0,0 +1,21 @@
+import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
+import { LogRepository } from "../../domain/repository/log.repository";
+import { LogDatasource } from '../../domain/datasources/log.datasources';
+
+
+export class LogRepositoryImpl implements LogRepository {
+
+    // inyeccion de dependencias
+    constructor(
+        private readonly logDatasource: LogDatasource,
+    ) {}
+
+    
+    async saveLog(log: LogEntity): Promise<void> {
+        return this.logDatasource.saveLog( log );
+    }
+    async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+        return this.logDatasource.getLogs( severityLevel );
+    }
+
+}
