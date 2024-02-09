6bbf6563498ada747387c4eb02262e220551a172
Paul Pichon F <paul10_barca@hotmail.com>
2024-02-09 15:46:54 -0600

    Create: creacion de logica

--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -9,7 +9,7 @@ import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
 export class FileSystemDatasource implements LogDatasource{
     // 
     private readonly logPath = 'logs/';
-    private readonly allLogsPath = 'logs/logs-low.log';
+    private readonly allLogsPath = 'logs/logs-all.log';
     private readonly mediumLogsPath = 'logs/logs-medium.log';
     private readonly highLogsPath = 'logs/logs-high.log';
 
@@ -42,8 +42,24 @@ export class FileSystemDatasource implements LogDatasource{
     }
 
 
-    saveLog(log: LogEntity): Promise<void> {
-        throw new Error("Method not implemented.");
+    async saveLog( newLog: LogEntity): Promise<void> {
+        // constante para crear log a JSON
+        const logAsJson = `${ JSON.stringify( newLog ) }\n`;
+
+        // grabar logs
+        fs.appendFileSync( this.allLogsPath, logAsJson );
+        // verificar si el level es identicamente igual a low level
+        // si es igual entonces ponemos un return;
+        if ( newLog.level === LogSeverityLevel.low) return;
+        // medium
+        // si level es igual a medium
+        if ( newLog.level === LogSeverityLevel.medium) {
+            // lo grabamos
+            fs.appendFileSync( this.mediumLogsPath, logAsJson );
+        } else {
+            // en el caso que no sea ni low ni medium entonces lo grabamos en HIGH
+            fs.appendFileSync( this.highLogsPath, logAsJson );
+        }
     }
     getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
         throw new Error("Method not implemented.");
