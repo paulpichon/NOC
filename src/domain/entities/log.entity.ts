0644bf55f82d88fa8ff67d2253cb5257ac60c813
Paul Pichon F <paul10_barca@hotmail.com>
2024-02-12 19:11:04 -0600

    Create: creacion de nueva propiedad

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -8,6 +8,14 @@ export enum LogSeverityLevel {
     high= 'high'
 }
 
+// options
+export interface LogEntityOptions {
+    level: LogSeverityLevel;
+    message: string;
+    origin: string;
+    createdAt?: Date;
+}
+
 
 // Aqui tendremos lo que queremos registrar en nuestra aplicacion
 export class LogEntity {
@@ -15,23 +23,30 @@ export class LogEntity {
     public level: LogSeverityLevel; //Enum
     public message: string;
     public createdAt: Date;
+    public origin: string;
 
     // constructor
-    constructor(message: string, level: LogSeverityLevel) {
+    constructor( options: LogEntityOptions) {
+        // desestructurar options
+        const { message, level, origin, createdAt = new Date() } = options;
         // inicializar
         this.message = message;
         this.level = level;
-        this.createdAt = new Date();
+        this.createdAt = createdAt;
+        this.origin = origin;
     }
 
     // metodo para parsear el JSON
     static fromJson = ( json: string ): LogEntity => {
         // desestructuramos de JSON.parse( json )
-        const { message, level, createdAt } = JSON.parse( json );
+        const { message, level, createdAt, origin } = JSON.parse( json );
         // creamos el log
-        const log = new LogEntity( message, level );
-        //  agregamos la fecha
-        log.createdAt = new Date( createdAt );
+        const log = new LogEntity( {
+            message, 
+            level,
+            createdAt,
+            origin
+        } );
         // retornamos el log
         return log;
 
