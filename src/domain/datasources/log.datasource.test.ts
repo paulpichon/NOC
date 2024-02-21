80797f346fb1ddf536f01da7dd18fc5f6bd26652
Paul Pichon F <paul10_barca@hotmail.com>
2024-02-21 14:25:50 -0600

    Create: pruebas en log.datasource.test.ts

---
+++ src/domain/datasources/log.datasource.test.ts
@@ -0,0 +1,36 @@
+import { LogEntity, LogSeverityLevel } from "../entities/log.entity";
+import { LogDatasource } from "./log.datasources";
+
+describe('log.datasource.test.ts LogDatasource', () => {
+
+    // log temporal
+    const newLog = new LogEntity({
+        origin: 'log.datasource.test.ts',
+        message: 'test-message',
+        level: LogSeverityLevel.low
+    });
+
+    class MockLogDatasource implements LogDatasource {
+        async saveLog(log: LogEntity): Promise<void> {
+            return;
+        }
+        async getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+            return[newLog];
+        }
+
+    }
+
+    test('Sould test the abstract class', async () => {
+        const mockLogDatasource = new MockLogDatasource();
+        expect( mockLogDatasource ).toBeInstanceOf( MockLogDatasource );
+        expect( typeof mockLogDatasource.saveLog ).toBe( 'function' );
+        expect( typeof mockLogDatasource.getLogs ).toBe( 'function' );
+
+        await mockLogDatasource.saveLog( newLog );
+        const logs = await mockLogDatasource.getLogs( LogSeverityLevel.high );
+        expect( logs ).toHaveLength( 1 );
+        expect( logs[ 0 ] ).toBeInstanceOf( LogEntity );
+    });
+
+
+});
